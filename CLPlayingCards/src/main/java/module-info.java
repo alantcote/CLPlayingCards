01e18db1a2f2afd6f1f708701b77f66758ea68f7
Alan T. Cote' <alantcote@gmail.com>
2021-12-28 17:23:22 -0500

    GOT to get target directory into .gitignore.

--- CLPlayingCards/src/main/java/module-info.java
+++ CLPlayingCards/src/main/java/module-info.java
@@ -3,6 +3,9 @@
  */
 module playingcards {
 	exports net.sf.cotelab.playingcards;
+	exports net.sf.cotelab.playingcards.decks;
+	exports net.sf.cotelab.playingcards.demo;
+	exports net.sf.cotelab.playingcards.javafx;
 	
 	requires javafx.graphics;
 }
--- CLPlayingCards/target/apidocs/package-search-index.js
+++ CLPlayingCards/target/apidocs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"playingcards","l":"net.sf.cotelab.playingcards"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"playingcards","l":"net.sf.cotelab.playingcards"},{"m":"playingcards","l":"net.sf.cotelab.playingcards.decks"},{"m":"playingcards","l":"net.sf.cotelab.playingcards.demo"},{"m":"playingcards","l":"net.sf.cotelab.playingcards.javafx"}];updateSearchResults();
